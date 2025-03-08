c091827ef007f1924911d65b051c15b41e0c722e
Dhruv Kumar Jaiswal <rishidhruv999@gmail.com>
2025-03-08 18:08:35 +0530

    yes it is latest

--- routes/auth.js
+++ routes/auth.js
@@ -277,7 +277,7 @@ router.post('/logout', (req, res) => {
 // Fetch children data
 router.get('/children', auth, async (req, res) => {
   try {
-    const children = await User.find({ parentReferralCode: req.user.referralCode }).select('name referralCode level commission');
+    const children = await User.find({ parentReferralCode: req.user.referralCode, role: 'associate' }).select('name referralCode level commission');
     res.json(children);
   } catch (err) {
     console.error(err.message);
